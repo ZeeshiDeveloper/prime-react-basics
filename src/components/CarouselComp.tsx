6083c95464a891746a43b4e5bc7b0b77108d6b2f
Zeeshan Ali <zee03044163645@gmail.com>
2022-11-16 17:10:11 +0500

    Make Some changes in styles of Carousel.

--- src/components/CarouselComp.tsx
+++ src/components/CarouselComp.tsx
@@ -23,15 +23,17 @@ const CarouselComp = () => {
     ];
     const data = [
         {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
-        {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
-        {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
-        {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
-        {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
-        {id: "1000",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1001",code: "f230fh0g3",name: "Blue T-Shirt",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/blue-t-shirt.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1002",code: "f230fh0g3",name: "Bracelet",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bracelet.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1003",code: "f230fh0g3",name: "Blue Band",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/blue-band.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1004",code: "f230fh0g3",name: "Black Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/black-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1005",code: "f230fh0g3",name: "Brown Purse",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/brown-purse.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1006",code: "f230fh0g3",name: "Game Controller",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/game-controller.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
+        {id: "1007",code: "f230fh0g3",name: "Bamboo Watch",description: "Product Description",image: "https://www.primefaces.org/primereact/images/product/bamboo-watch.jpg",price: 65,category: "Accessories",quantity: 24,inventoryStatus: "INSTOCK",rating: 5},
     ]
     const productTemplate = (data: { image: any; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; price: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; inventoryStatus: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | null | undefined; }) => {
         return (
-            <div className="border-1 p-4 border-round border-black-alpha-30 m-4">
+            <div className="border-1 p-4 border-round border-black-alpha-30 m-2">
                 <div className="">
                     <div className="mb-3 flex flex-column align-items-center justify-content-center">
                         <img src={data.image}  className="product-image" />
@@ -51,10 +53,10 @@ const CarouselComp = () => {
         );
     }
   return (
-    <div>
+    <div className='w-9 mx-auto'>
         <h1 className='text-center text-green-700 border-bottom-2 w-21rem flex justify-content-center mx-auto'>Carousel Component</h1>
-        <Carousel value={data} numVisible={3} numScroll={1} responsiveOptions={responsiveOptions} className="custom-carousel" circular
-                itemTemplate={productTemplate} header={<h3>Circular, 3 Items per Page and Scroll by 1</h3>} />
+        <Carousel value={data} numVisible={3} numScroll={1} responsiveOptions={responsiveOptions} 
+                itemTemplate={productTemplate} header={<h2 className='ml-5 text-primary bg-gray-200 p-3 w-4 border-round hover:shadow-2 transition-colors transition-duration-1000 hover:bg-gray-400'>3 Items per Page and Scroll by 1</h2>} />
         <DividerDark/>
     </div>
   )
